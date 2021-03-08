1a30ce8c435dc4e654691a7a935e9ee00d4d35a7
Tomabage <tamaoona@163.com>
2021-03-08 14:56:40 +0800

    创建button-group组件

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
+import ButtonGroup from './button-group'
 
 Vue.component('e-button', Button)
-Vue.component('e-icon',Icon)
+Vue.component('e-icon', Icon)
+Vue.component('e-button-group', ButtonGroup)
 
 new Vue({
     el: '#app',
-    data:{
-        loading1:false,
-        loading2:true,
-        loading3:false,
+    data: {
+        loading1: false,
+        loading2: true,
+        loading3: false,
     }
 })
---
+++ src/button-group.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="e-button-group">
+    <slot/>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+name: "button-group"
+}
+</script>
+
+<style lang="scss" scoped>
+.e-button-group{
+  display: inline-flex;
+  vertical-align: middle;
+  > .e-button{
+    border-radius:0;
+    margin-left: -1px;
+    &:first-child{
+      border-top-left-radius: var(--border-radius);
+      border-bottom-left-radius: var(--border-radius);
+    }
+    &:last-child{
+      border-top-right-radius: var(--border-radius);
+      border-bottom-right-radius: var(--border-radius);
+    }
+    &:hover{
+      position: relative;
+      z-index: 1;
+    }
+  }
+}
+</style>
