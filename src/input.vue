f1c15257b3d730d039b08ebfab3a9384f08065a1
Tomabage <tamaoona@163.com>
2021-03-12 11:47:04 +0800

    添加input组件及其样式

---
+++ src/input.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="wrapper" :class="{error}">
+    <input :value="value" type="text" :readonly="readonly" :disabled="disabled">
+    <template v-if="error">
+      <icon name="error" class="icon-error"/>
+      <span class="errorMessage">{{ error }}</span>
+    </template>
+  </div>
+</template>
+
+<script lang="ts">
+import Icon from './icon.vue'
+
+export default {
+  name: 'e-input',
+  components: {Icon},
+  props: {
+    value: {
+      type: String
+    },
+    readonly: {
+      type: Boolean,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    error: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+$height: 32px;
+$border-color: #999;
+$border-color-hover: #666;
+$border-radius: 4px;
+$font-size: 12px;
+$box-shadow-color: rgba(0, 0, 0, .5);
+$red: #F1453D;
+.wrapper {
+  font-size: $font-size;
+  display: inline-flex;
+  align-items: center;
+
+  > :not(:last-child) {
+    margin-right: .5em;
+  }
+
+  > input {
+    height: $height;
+    border: 1px solid $border-color;
+    border-radius: $border-radius;
+    padding: 0 8px;
+    font-size: inherit;
+
+    &:hover {
+      border-color: $border-color-hover;
+    }
+
+    &:focus {
+      box-shadow: inset 0 1px 3px $box-shadow-color;
+      outline: none;
+    }
+
+    &[disabled], &[readonly] {
+      border-color: #bbb;
+      color: #bbb;
+      cursor: not-allowed;
+    }
+
+    &.error {
+      > input {
+        border-color: $red;
+      }
+    }
+  }
+
+  .icon-error {
+    fill: $red
+  }
+
+  .errorMessage {
+    color: $red
+  }
+}
+</style>
