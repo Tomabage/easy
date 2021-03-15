82937ed976f4acc69d57fbb733172fafea26d972
Tomabage <tamaoona@163.com>
2021-03-15 16:34:56 +0800

    基本实现row组件和col组件功能

--- src/col.vue
+++ src/col.vue
@@ -1,29 +1,50 @@
 <template>
-  <div class="col" :class="[`col-$(span)`]">
-    <slot></slot>
+  <div class="col" :class="[span && `col-${span}`,offset && `offset-${offset}`]"
+       :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}">
+    <div style="border: 1px solid lightskyblue; height: 100px;">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  props:{
-    span:{
-      type:[Number,String]
+  props: {
+    span: {
+      type: [Number, String]
+    },
+    offset: {
+      type: [Number, String]
     }
+  },
+  data() {
+    return {
+      gutter: 0
+    }
+  },
+  created() {
+    console.log('col created')
+  },
+  mounted() {
+    console.log('col mounted')
   }
+
 }
 </script>
 
 <style lang="scss" scoped>
-.col{
-  height: 100px;
-  background: lightskyblue;
+.col {
   width: 50%;
-  border: 1px solid black;
   $class-prefix: col-;
-  @for $n from 1 through 24{
-    &.#{$class-prefix}#{$n}{
-      width:($n / 24) * 100%
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      width: ($n / 24) * 100%
+    }
+  }
+  $class-prefix: offset-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      margin-left: ($n / 24) *100%;
     }
   }
 }
