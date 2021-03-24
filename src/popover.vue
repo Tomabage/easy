3265b2a6f768ea0ce69e0feb6f027efc473c009f
Tomabage <tamaoona@163.com>
2021-03-24 15:23:03 +0800

    完成div卡片的样式

--- src/popover.vue
+++ src/popover.vue
@@ -3,7 +3,7 @@
     <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
-    <span ref="triggerWrapper">
+    <span ref="triggerWrapper" style="display: inline-block;">
           <slot></slot>
     </span>
   </div>
@@ -20,13 +20,18 @@ export default {
   methods: {
     positionContent() {
       document.body.appendChild(this.$refs.contentWrapper)
-      let {width, height, left, top} = this.$refs.triggerWrapper.getBoundingClientRect()
+      let {left, top} = this.$refs.triggerWrapper.getBoundingClientRect()
       this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
       this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
     },
     onClickDocument(e) {
       if (this.$refs.popover &&
           (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))
+      ) {
+        return
+      }
+      if (this.$refs.contentWrapper &&
+          (this.$refs.contentWrapper === e.target || this.$refs.contentWrapper.contains(e.target))
       ) {
         return
       }
@@ -57,6 +62,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$border-color: #333;
+$border-radius: 4px;
 .popover {
   display: inline-block;
   vertical-align: top;
@@ -65,8 +72,34 @@ export default {
 
 .content-wrapper {
   position: absolute;
-  border: 1px solid #1890ff;
-  box-shadow: 0 0 3px rgba(0, 0, 0, .5);
+  border: 1px solid $border-color;
+  border-radius: $border-radius;
+  filter: drop-shadow(0 1px 1px rgba(0, 0, 0, .5));
+  background: white;
   transform: translateY(-100%);
+  margin-top: -10px;
+  padding: .5em 1em;
+  max-width: 20em;
+  word-break: break-all;
+
+  &::before, &::after {
+    content: '';
+    display: block;
+    border: 10px solid transparent;
+    width: 0;
+    height: 0;
+    position: absolute;
+    left: 10px;
+  }
+
+  &::before {
+    border-top-color: black;
+    top: 100%;
+  }
+
+  &::after {
+    border-top-color: white;
+    top: calc(100% - 1px);
+  }
 }
 </style>
