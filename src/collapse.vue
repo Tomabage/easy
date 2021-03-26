549b67a07b758b884c51648dc57daca0ae52b3f4
Tomabage <tamaoona@163.com>
2021-03-26 10:40:11 +0800

    实现手风琴效果

--- src/collapse.vue
+++ src/collapse.vue
@@ -1,19 +1,39 @@
 <template>
-    <div class="collapse">
-      <slot></slot>
-    </div>
+  <div class="collapse">
+    <slot></slot>
+  </div>
 </template>
 
 <script>
+import Vue from 'vue'
+
 export default {
-name: "EasyCollapse"
+  name: "EasyCollapse",
+  data() {
+    return {
+      eventBus: new Vue()
+    }
+  },
+  props: {
+    single: {
+      type: Boolean,
+      default: false
+    }
+  },
+  provide() {
+    if (this.single) {
+      return {
+        eventBus: this.eventBus
+      }
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-$grey:grey;
-$border-radius:4px;
-.collapse{
+$grey: grey;
+$border-radius: 4px;
+.collapse {
   border: 1px solid $grey;
   border-radius: $border-radius;
 }
